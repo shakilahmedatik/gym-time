dde7e0d6b7b38e669901832f0a49a757ba2a8679
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-03 18:28:24 +0600

    Workout data & Item component added

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,22 +1,27 @@
-import { useEffect } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import Navigation from '../navigation'
+import data from '../data.json'
+import { NativeStackHeaderProps } from '@react-navigation/native-stack'
+import { Workout } from '../types/data'
+import WorkoutItem from '../components/WorkoutItem'
 
-const HomeScreen = ({ navigation }: any) => {
-  useEffect(() => {
-    console.log('Rendering Home Screen')
-
-    return () => console.log('Unmounting Home Screen')
-  }, [])
+const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>I am home component</Text>
-      <Button
-        title='Go to Planner'
-        onPress={() => navigation.navigate('Planner')}
+      <FlatList
+        data={data as Workout[]}
+        renderItem={WorkoutItem}
+        keyExtractor={item => item.slug}
       />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+})
 
 export default HomeScreen
---
+++ types/data.ts
@@ -0,0 +1,19 @@
+
+export type Difficulty = "easy" | "normal" | "hard"
+export type SequenceType = "exercise" | "break" | "stretch"
+
+export interface Workout{
+    slug: string,
+    name: string,
+    duration: number,
+    difficulty: Difficulty,
+    sequence: Array<SequenceItem>
+}
+
+export interface SequenceItem {
+    slug: string,
+    name: string,
+    type: SequenceType,
+    reps?: number,
+    duration: number
+}
