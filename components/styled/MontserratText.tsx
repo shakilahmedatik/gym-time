b31a28e156ad6c0284bf5c293d94a368e144307d
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-04-19 23:58:02 +0600

    Update MontserratText.tsx

--- components/styled/MontserratText.tsx
+++ components/styled/MontserratText.tsx
@@ -4,3 +4,12 @@ import { Text } from 'react-native'
 export function MontserratText(props: Text['props']) {
   return <Text {...props} style={[props.style, { fontFamily: 'montserrat' }]} />
 }
+
+
+
+
+
+
+
+
+
